bf0bb6668de6533fd1184ab0b62bc7d927681f0b
Ellen Gong <ellen3gz@gmail.com>
2018-06-09 11:50:29 +1000

    configure security to disable authentication for now

--- src/main/java/com/ellen/seckill/config/WebSecurityConfig.java
+++ src/main/java/com/ellen/seckill/config/WebSecurityConfig.java
@@ -13,5 +13,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .csrf().disable();
+        http
+                .authorizeRequests()
+                .anyRequest()
+                .permitAll();
     }
 }
